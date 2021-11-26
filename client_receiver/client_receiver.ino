ff65529ea0c53b458a052310507445e91cbade30
Serenetic <31093073+Serenetic@users.noreply.github.com>
2021-11-26 17:45:54 +0000

    Hit detection and height tracking

--- client_receiver/client_receiver.ino
+++ client_receiver/client_receiver.ino
@@ -1,6 +1,6 @@
 #include <Wire.h>
 #include <LiquidCrystal.h>
-#include "./Libraries/Ultrasonic.h"
+#include "Ultrasonic.h"
 
 const byte deviceAddress = 1;
 
@@ -8,7 +8,9 @@ const int pressurePadPin = 3;
 const int rs = 12, en = 11, d4 = 6, d5 = 7, d6 = 8, d7 = 9;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
-volatile int score = 0;
+bool prevPressed = false;
+int score = 0;
+volatile byte currentNote = 0;
 
 Ultrasonic ultrasonic(5);
 
@@ -18,47 +20,58 @@ void setup()
   Wire.onReceive(receiveEvent);
   Serial.begin(9600);
   pinMode(3, INPUT_PULLUP);
-  attachInterrupt(digitalPinToInterrupt(pressurePadPin), pressurePadHit, FALLING);
 
   lcd.begin(16, 2);
 }
 
 void loop()
 {
+  lcd.setCursor(0, 0);
+  lcd.print(currentNote);
   lcd.setCursor(0, 1);
-  
-  delay(500);
-  Wire.beginTransmission(4);
-  Wire.write(deviceAddress);
-  Wire.write(13);
-  Wire.endTransmission();
-}
-
-void pressurePadHit()
-{
-  score += ultrasonic.MeasureInCentimeters();
-  lcd.clear();
   lcd.print(score);
+  lcd.setCursor(12, 0);
+  lcd.print(ultrasonic.MeasureInCentimeters());
+
+  if (digitalRead(pressurePadPin) == false && !prevPressed)
+  {
+    prevPressed = true;
+    if (currentNote == 0)
+    {
+      score--;
+    }
+    else
+    {
+      currentNote = 0;
+      score++;
+    }
+  }
+  else if (digitalRead(pressurePadPin) == true && prevPressed)
+  {
+    prevPressed = false;
+  }
 }
 
 void receiveEvent(int howMany)
 {
   byte b = Wire.read();
 
-  if (b == deviceAddress or b == 0)
+  if (b == deviceAddress)
   {
     while(Wire.available())
     {
-      int i = Wire.read();
-      lcd.clear();
-      lcd.print(i);
+      byte i = Wire.read();
     }
+  }
+  else if (b == 0)
+  {
+    currentNote = Wire.read();
   }
   else
   {
     while(Wire.available())
     {
-      int i = Wire.read();
+      byte i = Wire.read();
     }
   }
 }
