6624208d0d30151fc558e9b562a66f17945adb33
EzaEx <tomlewisint@gmail.com>
2021-12-10 12:28:44 +0000

    move reciever, replace old

--- client_receiver/client_receiver.ino
+++ client_receiver/client_receiver.ino
@@ -2,6 +2,7 @@
 #include <LiquidCrystal.h>
 #include "Ultrasonic.h"
 
+// need to change this variable for each client (1,2,3)
 const byte deviceAddress = 1;
 
 const int pressurePadPin = 3;
@@ -12,6 +13,10 @@ bool prevPressed = false;
 int score = 0;
 volatile byte currentNote = 0;
 
+unsigned long timeReceived = 0;
+
+//bool playingSong = false; variable we might need later?
+
 Ultrasonic ultrasonic(5);
 
 void setup()
@@ -20,29 +25,41 @@ void setup()
   Wire.onReceive(receiveEvent);
   Serial.begin(9600);
   pinMode(3, INPUT_PULLUP);
-
+  
   lcd.begin(16, 2);
+  lcd.setCursor(3, 0);
+  lcd.print("PLAYER ");
+  lcd.setCursor(10,0);
+  lcd.print(deviceAddress);
+  
 }
 
 void loop()
 {
   lcd.setCursor(0, 0);
+  lcd.print(currentNote);
   lcd.setCursor(0, 1);
   lcd.print(score);
   lcd.setCursor(12, 0);
   int height = ultrasonic.MeasureInCentimeters();
-  lcd.print(height);
+  if (height/5 < 1000) lcd.print(" ");
+  if (height/5 < 100) lcd.print(" ");
+  if (height/5 < 10) lcd.print(" ");
+  lcd.print(height/5);
 
   if (digitalRead(pressurePadPin) == false && !prevPressed)
   {
     prevPressed = true;
+    unsigned long timePressed = millis();
     if (currentNote == 0)
     {
       score -= 300;
     }
     else
     {
-      int points = 1000 - (abs(currentNote - height/5) * 150);
+      // calculate points in regards to the ultrasensor and how fast the player was
+      unsigned long playerSpeed = timePressed - timeReceived;
+      int points = 1000 - min(abs(currentNote - height/5) * 200 - playerSpeed, 1000);
       currentNote = 0;
       score += points;
     }
@@ -55,6 +72,7 @@ void loop()
 
 void receiveEvent(int howMany)
 {
+  timeReceived = millis();
   byte b = Wire.read();
   Serial.println(b);
 
